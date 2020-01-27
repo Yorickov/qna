240e0a4c320fa34d219c26ce8910c2334908d36f
yorickov <yorickov@gmail.com>
2020-01-27 20:44:59 +0300

    add C-Questions #index #show

--- Gemfile
+++ Gemfile
@@ -38,7 +38,8 @@ gem 'aasm'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails', '~> 3.9'
+  gem 'rspec-rails', '4.0.0.beta3'
+  gem 'rails-controller-testing'
   gem 'factory_bot_rails', '~> 5.1'
 end
 
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,23 @@ module Qna
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # remove active-storage action-mailer action-mailbox from rails routes
+    initializer(
+      :remove_action_mailbox_and_activestorage_routes,
+      after: :add_routing_paths
+    ) do |app|
+      app.routes_reloader.paths.delete_if { |path| path =~ /activestorage/ }
+      app.routes_reloader.paths.delete_if { |path| path =~ /actionmailbox/ }
+    end
+
+    # config generators for tests
+    config.generators do |g|
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false
+    end
   end
 end
