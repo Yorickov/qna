c011e4a3b0b01dcdf5c1bf23399898561f24949c
yorickov <yorickov@gmail.com>
2020-01-28 00:38:32 +0300

    add AC-Answers #new, #create

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -28,4 +28,60 @@ RSpec.describe AnswersController, type: :controller do
       expect(response).to render_template :show
     end
   end
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new answer in the database' do
+        expect do
+          post :create, params: {
+            question_id: question,
+            answer: attributes_for(:answer)
+          }
+        end
+          .to change(Answer, :count).by(4)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: {
+          question_id: question,
+          answer: attributes_for(:answer)
+        }
+
+        expect(response).to redirect_to assigns(:answer)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect do
+          post :create, params: {
+            question_id: question,
+            answer: attributes_for(:answer, :invalid)
+          }
+        end
+          .to change(Answer, :count).by(3)
+      end
+
+      it 're-renders new view' do
+        post :create, params: {
+          question_id: question,
+          answer: attributes_for(:answer, :invalid)
+        }
+
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
