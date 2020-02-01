f491c0aa1788ff94624a4b5ef6716bf1ba0af958
yorickov <yorickov@gmail.com>
2020-02-02 02:04:32 +0300

    fix controller-tests

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,61 +2,75 @@ require 'rails_helper'
 
 describe AnswersController, type: :controller do
   let(:user) { build(:user_with_questions) }
+  let(:no_author) { build(:user) }
 
   describe 'POST #create' do
     let(:answer) { create(:answer) }
     let(:question) { user.questions.first }
 
-    before { login(user) }
+    context 'as User' do
+      before { login(user) }
 
-    context 'with valid attributes' do
-      it 'saves a new answer in the database' do
-        expect do
+      context 'with valid attributes' do
+        it 'saves a new answer in the database' do
+          expect do
+            post :create, params: {
+              question_id: question,
+              answer: attributes_for(:answer)
+            }
+          end
+            .to change(Answer, :count).by(1)
+        end
+
+        it 'saves as the answer of correct question' do
           post :create, params: {
             question_id: question,
             answer: attributes_for(:answer)
           }
+
+          expect(Answer.order(:created_at).last.question).to eq(question)
         end
-          .to change(Answer, :count).by(1)
+
+        it 'redirects to show view' do
+          post :create, params: {
+            question_id: question,
+            answer: attributes_for(:answer)
+          }
+
+          expect(response).to redirect_to question
+        end
       end
 
-      it 'saves as the answer of correct question' do
-        post :create, params: {
-          question_id: question,
-          answer: attributes_for(:answer)
-        }
+      context 'with invalid attributes' do
+        it 'does not save the answer' do
+          expect do
+            post :create, params: {
+              question_id: question,
+              answer: attributes_for(:answer, :invalid)
+            }
+          end
+            .to change(Answer, :count).by(0)
+        end
 
-        expect(Answer.order(:created_at).last.question).to eq(question)
-      end
-
-      it 'redirects to show view' do
-        post :create, params: {
-          question_id: question,
-          answer: attributes_for(:answer)
-        }
-
-        expect(response).to redirect_to question
-      end
-    end
-
-    context 'with invalid attributes' do
-      it 'does not save the answer' do
-        expect do
+        it 're-renders new view' do
           post :create, params: {
             question_id: question,
             answer: attributes_for(:answer, :invalid)
           }
-        end
-          .to change(Answer, :count).by(0)
-      end
 
-      it 're-renders new view' do
+          expect(response).to render_template 'questions/show'
+        end
+      end
+    end
+
+    context 'as Guest' do
+      it 'redirects to new session' do
         post :create, params: {
           question_id: question,
           answer: attributes_for(:answer, :invalid)
         }
 
-        expect(response).to render_template 'questions/show'
+        expect(response).to redirect_to new_user_session_path
       end
     end
   end
@@ -66,17 +80,37 @@ describe AnswersController, type: :controller do
       create(:answer, question: user.questions.first, user: user)
     end
 
-    before { login(user) }
+    context 'as authorized Author' do
+      before { login(user) }
 
-    it 'deletes the answer' do
-      expect { delete :destroy, params: { id: answer } }
-        .to change(Answer, :count).by(-1)
+      it 'deletes the answer' do
+        expect { delete :destroy, params: { id: answer } }
+          .to change(Answer, :count).by(-1)
+      end
+
+      it 'redirects to show' do
+        delete :destroy, params: { id: answer }
+
+        expect(response).to redirect_to answer.question
+      end
     end
 
-    it 'redirects to show' do
-      delete :destroy, params: { id: answer }
+    context 'as authorized no Author' do
+      it 'redirects to root' do
+        login(no_author)
 
-      expect(response).to redirect_to answer.question
+        delete :destroy, params: { id: answer }
+
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'as Guest' do
+      it 'redirects to new session' do
+        delete :destroy, params: { id: answer }
+
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 end
