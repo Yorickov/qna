cde244171a7c1078fa718d8feec67a212bfaef05
yorickov <yorickov@gmail.com>
2020-02-02 00:56:07 +0300

    fix registration and authorization tests

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-feature 'User can sign in' do
+feature 'Registered user can sign in' do
   given(:user) { build(:user) }
+  given(:wrong_data) { 'wrong data' }
 
   background { visit new_user_session_path }
 
-  scenario 'Registered user tries to sign in' do
+  scenario 'User tries to sign in with correct data' do
     within('#new_user') do
       fill_in t('activerecord.attributes.user.email'), with: user.email
       fill_in t('activerecord.attributes.user.password'), with: user.password
@@ -15,10 +16,21 @@ feature 'User can sign in' do
     expect(page).to have_content t('devise.sessions.signed_in')
   end
 
-  scenario 'Unregistered user tries to sign in' do
+  scenario 'User tries to sign in with wrong email' do
     within('#new_user') do
-      fill_in t('activerecord.attributes.user.email'), with: 'wrong@mail.com'
-      fill_in t('activerecord.attributes.user.password'), with: '12345678'
+      fill_in t('activerecord.attributes.user.email'), with: wrong_data
+      fill_in t('activerecord.attributes.user.password'), with: user.password
+      click_on t('devise.shared.links.sign_in')
+    end
+
+    expect(page).to have_content t('devise.failure.not_found_in_database',
+                                   authentication_keys: 'Email')
+  end
+
+  scenario 'User tries to sign in with wrong password' do
+    within('#new_user') do
+      fill_in t('activerecord.attributes.user.email'), with: user.email
+      fill_in t('activerecord.attributes.user.password'), with: wrong_data
       click_on t('devise.shared.links.sign_in')
     end
 
